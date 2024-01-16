fc9911565a6e41f8fc05a44abc10686ad213c1f6
Davin Turas <davin@rainsoftware.tech>
2024-01-16 12:44:40 -0800

    meow

---
+++ trial/intro.py
@@ -0,0 +1,16 @@
+print('Hello, Django Girls!')
+if 3 > 2:
+    print('It Works!')
+
+if 5 > 2:
+    print('5 is greater than 2')
+else:
+    ('5 is not greater than 2')
+
+name = 'Stupid'
+if name == 'Ola':
+    print('Hey, Ola!')
+elif name == 'Stupid':
+    print('Hey, Stupid!')
+else:
+    print('Hey, anonymous!')
